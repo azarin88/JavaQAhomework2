70057a177cd436c858f69bcce7cda762169ed1ef
Azarin Maxim <azarin.m88@gmail.com>
2026-05-08 13:26:32 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
-public class Main
-{
-    public static void main (String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         short account = 100;
         short replenishment = 200;
-        if (replenishment >= 1000)
-        {
+        if (replenishment >= 1000) {
             System.out.println("Баланс: " + (account + replenishment * (1 + 0.01)) + " рублей.");
             System.out.println("Бонус: " + replenishment * 0.01 + " рублей");
-        }
-        else
-        {
+        } else {
             System.out.println("Баланс: " + (account + replenishment) + " рублей.");
             System.out.println("Бонус начисляется при пополнении счета от 1000 рублей! ;)");
         }
